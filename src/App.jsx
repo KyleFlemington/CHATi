babde279e1a911983db94a600b653f3ee36f6ab6
Kyle <Kyleflemington@gmail.com>
2016-10-31 21:47:59 +0000

    init commitit

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import React, {Component} from 'react';
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <h1>CHAT i</h1>
     );
   }
 }
 export default App;
+	
